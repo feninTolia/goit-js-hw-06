9fe8bf8a0b94baf63d2598a78ba1e38ddfea4000
feninTolia <fenintolja@gmail.com>
2022-09-01 19:02:44 +0300

    update task 10

--- js/task-09.js
+++ js/task-09.js
@@ -2,4 +2,21 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-const 
+const rgb2hex = (rgb) =>
+  `#${rgb
+    .match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/)
+    .slice(1)
+    .map((n) => parseInt(n, 10).toString(16).padStart(2, '0'))
+    .join('')}`;
+
+const refs = {
+  randomBtn: document.querySelector('.change-color'),
+  colorTextSpan: document.querySelector('.color'),
+};
+
+refs.colorTextSpan.textContent = '#ffffff';
+
+refs.randomBtn.addEventListener('click', () => {
+  document.body.style.backgroundColor = getRandomHexColor();
+  refs.colorTextSpan.textContent = rgb2hex(document.body.style.backgroundColor);
+});
