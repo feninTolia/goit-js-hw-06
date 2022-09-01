63624ed95cbf1e4a3b0e46898aad2f8f40e498a5
feninTolia <fenintolja@gmail.com>
2022-09-01 18:29:24 +0300

    update task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
+const refs = {
+  form: document.querySelector('.login-form'),
+};
 
+const onFormSubmit = (event) => {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === '' || password.value === '') {
+    alert('Заповніть будь-ласка всі поля.');
+    return;
+  }
+
+  console.log({
+    email: email.value,
+    password: password.value,
+  });
+
+  refs.form.reset();
+};
+
+refs.form.addEventListener('submit', onFormSubmit);
