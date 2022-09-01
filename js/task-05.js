62ed10d80bf1388bdaa90e88aa3bb7e8a02bda67
feninTolia <fenintolja@gmail.com>
2022-09-01 14:16:47 +0300

    Update task-05.js

---
+++ js/task-05.js
@@ -0,0 +1,8 @@
+const refs = {
+  inputEl: document.querySelector('#name-input'),
+  outputEl: document.querySelector('#name-output'),
+};
+
+refs.inputEl.addEventListener('input', (event) => {
+  refs.outputEl.textContent = event.currentTarget.value;
+});
